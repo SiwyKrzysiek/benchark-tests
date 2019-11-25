b1815b30bd3751b147f43028688890eed13e9a2d
WinCubeML <mati031398@gmail.com>
2019-11-25 23:30:05 +0100

    MergeSort branch init 2

--- Code/src/main/java/MergeSort.java
+++ Code/src/main/java/MergeSort.java
@@ -1,2 +1,11 @@
+import java.util.List;
+
 public class MergeSort {
+    public List<Integer> divide() {
+        return null;
+    }
+
+    public List<Integer> merge(){
+        return null;
+    }
 }
