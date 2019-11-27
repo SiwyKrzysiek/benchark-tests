e72ab63d1570091eba97cbadff2c516e48f9bcd3
WinCubeML <mati031398@gmail.com>
2019-11-27 21:17:53 +0100

    MergeSort divide and sort methods

--- Code/src/main/java/MergeSort.java
+++ Code/src/main/java/MergeSort.java
@@ -1,11 +1,27 @@
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class MergeSort {
-    public List<Integer> divide() {
-        return null;
+    public List<Integer> sort(List<Integer> list) {
+        Integer[] array = new Integer[list.size()];
+        array = list.toArray(array);
+        divide(array, 0, list.size() - 1);
+        return new ArrayList<>(Arrays.asList(array));
     }
 
-    public List<Integer> merge(){
-        return null;
+    private void divide(Integer[] array, int l, int r) {
+        if (l < r) {
+            int m = (r + l) / 2;
+
+            divide(array, l, m);
+            divide(array, m + 1, r);
+
+            merge(array, l, m, r);
+        }
+    }
+
+    private void merge(Integer[] array, int l, int m, int r) {
+
     }
 }
