ce8cb55104ea5d6028333902fe282a29af3d2b37
Krzysztof Dąbrowski <01133318@pw.edu.pl>
2019-11-28 17:43:09 +0100

    Insertion sort Integer implementation

--- Code/src/main/java/InsertionSort.java
+++ Code/src/main/java/InsertionSort.java
@@ -1,9 +1,24 @@
 import java.util.Arrays;
 import java.util.List;
 
-public class InsertionSort<T extends Comparable> implements SortingAlgorithm<T> {
+public class InsertionSort implements SortingAlgorithm<Integer> {
+
     @Override
-    public List<T> sort(List<T> data) {
-        return data;
+    public List<Integer> sort(List<Integer> data) {
+        Integer[] array = data.toArray(new Integer[0]);
+
+        for (int i = 1; i < array.length; i++) {
+            Integer current = array[i]; //Save current
+
+            int j = i-1;
+            while (j >= 0 && array[j].compareTo(current) > 0) {
+                array[j + 1] = array[j]; //Shift table
+                j--;
+            }
+
+            array[j + 1] = current; //Put back
+        }
+        
+        return Arrays.asList(array);
     }
 }
