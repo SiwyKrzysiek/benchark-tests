5cd3f876c55f24da925be16e37062a0a36029935
Krzysztof Dąbrowski <01133318@pw.edu.pl>
2019-11-28 17:48:40 +0100

    Proper generalization to Comparable

--- Code/src/main/java/InsertionSort.java
+++ Code/src/main/java/InsertionSort.java
@@ -1,14 +1,14 @@
 import java.util.Arrays;
 import java.util.List;
 
-public class InsertionSort implements SortingAlgorithm<Integer> {
+public class InsertionSort<T extends Comparable> implements SortingAlgorithm<T> {
 
     @Override
-    public List<Integer> sort(List<Integer> data) {
-        Integer[] array = data.toArray(new Integer[0]);
+    public List<T> sort(List<T> data) {
+        Comparable[] array = data.toArray(new Comparable[0]);
 
         for (int i = 1; i < array.length; i++) {
-            Integer current = array[i]; //Save current
+            Comparable current = array[i]; //Save current
 
             int j = i-1;
             while (j >= 0 && array[j].compareTo(current) > 0) {
@@ -18,7 +18,7 @@ public class InsertionSort implements SortingAlgorithm<Integer> {
 
             array[j + 1] = current; //Put back
         }
-        
-        return Arrays.asList(array);
+
+        return Arrays.asList((T[]) array);
     }
 }
