1502569e3dc849d73a2e05df8f7707f401ad2a71
Krzysztof Dąbrowski <01133318@pw.edu.pl>
2019-12-02 13:58:31 +0100

    Uwzględnienie algorytmu sortowania

--- Code/src/main/java/searching/BisectionSearch.java
+++ Code/src/main/java/searching/BisectionSearch.java
@@ -1,8 +1,20 @@
 package searching;
 
+import sorting.SortingAlgorithm;
+
 import java.util.List;
 
 public class BisectionSearch<T extends Comparable> {
+
+    private final SortingAlgorithm<T> sortingAlgorithm;
+
+    /**
+     *
+     * @param sortingAlgorithm Algorithm used to initially sort data
+     */
+    public BisectionSearch(SortingAlgorithm<T> sortingAlgorithm) {
+        this.sortingAlgorithm = sortingAlgorithm;
+    }
 
     /**
      * Find object in list
@@ -10,7 +22,10 @@ public class BisectionSearch<T extends Comparable> {
      * @param value value that is equal to the one being searched for by <c>compare()</c>
      * @return found value from the list or <c>null</c> if not found
      */
-    T find(List<T> data, T value) {
+    public T find(List<T> data, T value) {
+        int startIndex = 0;
+        int endIndex = data.size() - 1;
+
         return null;
     }
 }
