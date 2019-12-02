d16d252ab7e000608bcf315b9cb87294fccadb58
Krzysztof Dąbrowski <01133318@pw.edu.pl>
2019-12-02 13:44:44 +0100

    Interface bisearcha

---
+++ Code/src/main/java/searching/BisectionSearch.java
@@ -0,0 +1,16 @@
+package searching;
+
+import java.util.List;
+
+public class BisectionSearch<T extends Comparable> {
+
+    /**
+     * Find object in list
+     * @param data  not sorted list
+     * @param value value that is equal to the one being searched for by <c>compare()</c>
+     * @return found value from the list or <c>null</c> if not found
+     */
+    T find(List<T> data, T value) {
+        return null;
+    }
+}
